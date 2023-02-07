38de9806b34dba3cf0c2367655125219ffd9f0b5
enzodmrib <enzorib2@gmail.com>
2023-02-07 05:19:53 -0300

    Adds final request parameter handling

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http" // Internal libraries require "node:" prefix
-import { Database } from "./middlewares/database.js"
 import { json } from "./middlewares/json.js"
-import { randomUUID } from 'node:crypto'
+import { routes } from "./routes.js"
+import { extractQueryParams } from "./utils/extract-query-params.js"
 
 // CommonJS => require
 // ESModules => import/export
@@ -11,32 +11,25 @@ import { randomUUID } from 'node:crypto'
 
 // Statefull application => has in memory data
 // (therefore stateless doesnt)
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res
-      .end(JSON.stringify(users))
-  }
+  if (route) {
+    const routeParams = req.url.match(route.path)
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+    const { query, ...params } = routeParams.groups
 
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
