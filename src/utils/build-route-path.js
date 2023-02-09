f65592489b701849e7226b0d7afc4555872b00c2
enzodmrib <enzorib2@gmail.com>
2023-02-09 03:58:11 -0300

    Adds comments to Regex logic for better understanding

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,8 +1,12 @@
 // users/:id
 export function buildRoutePath(path) {
+  // recognizing route parameters with regex
   const routeParametersRegex = /:([a-zA-z]+)/g
+
+  // switching the route parameters for the actual regex
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
+  // generating the regex with the path created
   const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`) 
 
   return pathRegex
